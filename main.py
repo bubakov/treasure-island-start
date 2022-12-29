089093c1095e3024614bd5eee87d86af6e18fc10
bubakov <115979905+bubakov@users.noreply.github.com>
2022-12-29 11:09:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("""Choose one of offered choice.
+If you choose another choice. Game over appears.""")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+# first player choose
+player_choose = input('You are on crossroad. Left or right? \n').lower()
+if player_choose == 'left':
+  # second player choice
+  player_choose_1 = input('You have come to a lake. Swim or wait for boat? \n').lower()
+  if player_choose_1 == 'wait':
+  # third player choice
+    player_choose_2 = input('Which door? Red, Yellow, Blue \n').lower()
+    if player_choose_2 == 'red':
+      print('Burned by fire. Game over.')
+    elif player_choose_2 == 'blue':
+      print('Eaten by beasts. Game over.')
+    elif player_choose_2 == 'yellow':
+      print('You found a treassure. You win.')
+    else:
+      print('Game over.')
+  else:
+    print('Attacked by trout. Game over.')
+else:
+  print('Fall into a hole. Game ower')
+
+
+
+
+
